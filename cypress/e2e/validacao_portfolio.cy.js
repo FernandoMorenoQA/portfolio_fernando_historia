2413523789fcd9649025983f09795b70ff7d075a
Fernando Celso <celsotesterestudos@gmail.com>
2026-03-25 11:21:39 -0300

    feat: configurando CI/CD do Cypress

--- cypress/e2e/validacao_portfolio.cy.js
+++ cypress/e2e/validacao_portfolio.cy.js
@@ -1,10 +1,22 @@
 describe('Smoke Test Simples', () => {
-  it('Deve carregar a página inicial', () => {
-    // Visita a URL e aguarda a resposta do servidor
-    cy.visit('https://portfoliofernandohistoria-wacedwvmyw6rub566bvfaa.streamlit.app/')
+  it('Deve validar a formação acadêmica dentro do iframe', () => {
+    cy.visit('https://portfoliofernandohistoria-wacedwvmyw6rub566bvfaa.streamlit.app/');
 
-    // Apenas verifica se o elemento "body" (corpo do site) existe e está visível
-    // O timeout de 15 segundos ajuda caso o Streamlit demore a "acordar"
-    cy.get('body', { timeout: 15000 }).should('be.visible')
-  })
-})
+    // 1. Espera o iframe carregar e acessa o corpo (body) dele
+    cy.get('iframe', { timeout: 20000 })
+      .its('0.contentDocument.body').should('not.be.empty')
+      .then(cy.wrap) // Agora o Cypress está "dentro" do iframe
+      .as('iframeBody');
+
+    // 2. Agora buscamos o texto dentro do corpo do iframe
+    cy.get('@iframeBody')
+      .contains('Mestre em TI', { timeout: 15000 })
+      .should('be.visible');
+
+    // 3. Validando o próximo badge para garantir a consistência
+    cy.get('@iframeBody')
+      .contains('MBA')
+      .should('be.visible');
+  });
+
+});
