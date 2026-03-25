9976e281f2171ac3a45c88f2ddd55b88b1cdd283
Fernando Celso <celsotesterestudos@gmail.com>
2026-03-25 15:55:11 -0300

    Corrigindo o link

--- cypress/e2e/github_link.cy.js
+++ cypress/e2e/github_link.cy.js
@@ -1,8 +1,8 @@
 describe('Validar Link do Repositório', () => {
     it('Deve verificar se o botão do GitHub aponta para o repositório correto', () => {
         // 1. Visita a página local ou de produção
-        cy.visit('https://portfoliofernandohistoria-wacedwvmyw6rub566bvfaa.streamlit.app/')
-
+        // Remova o _ERRO_DE_TESTE
+        cy.get('.btn-git').should('have.attr', 'href', 'https://github.com/FernandoMorenoQA/portfolio_fernando_historia')
         // 2. Localiza o botão pelo texto (ajuste se o texto for diferente)
         // Usamos o seletor 'a' porque o botão é um link estilizado no seu CSS
         cy.contains('Ver Código Fonte (Git)')
